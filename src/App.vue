2ed08b013f01543165e3ac98f49332cca52a7b02
RobbyR98 <robbyrunge@yahoo.com>
2025-08-10 20:42:48 +0200

    feat(card): implement Card component and update App.vue structure

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
-  <div id="app"></div>
+  <Card>
+    <h1>Willkommen zum Stock Dashboard</h1>
+    <p>Hier können Sie Ihre Aktien verfolgen.</p>
+  </Card>
 </template>
 
 <script>
+import Card from './components/Card.vue';
+
 export default {
   name: 'App',
   components: {
+    Card
   }
 }
 </script>
@@ -13,11 +19,11 @@ export default {
 <style>
 body {
   margin: 0;
-  padding: 0;
-  box-sizing: border-box;
 }
 
 #app {
+  padding: 100px;
+  box-sizing: border-box;
   background: radial-gradient(71.11% 100% at 50% 0%, #020204 14.6%, #011F35 100%);
   width: 100vw;
   min-height: 100vh;
---
+++ src/components/Card.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="card">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseCard'
+}
+</script>
+
+<style scoped>
+.card {
+  background-color: #011F35;
+  border-radius: 16px;
+  padding: 20px 32px 20px 32px;
+}
+</style>
